519d76abcaefa8c2173787cb4ba820fca0deeaa1
MIKE <mike.santoro42@gmail.com>
2019-01-08 18:47:02 -0500

    parsing payloads

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 var http = require('http');
 var url = require('url');    //node library
+var StringDecoder = require('string_decoder').StringDecoder;
 
 //The server should respond to all requests with a string 
 var server = http.createServer(function (req, res) {
@@ -23,12 +24,26 @@ var server = http.createServer(function (req, res) {
 	// Get HTTP Method
 	var method = req.method.toLowerCase();
 
-	// send the response 
-	res.end('Hello World\n');
+	// Get headers as an object
+	var headers = req.headers;
 
-	// log the requested path
-	console.log('Request recieved on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters ', queryStringObject);
+	//Get the payload  (strind decoder built in node library)
+	var decoder = new StringDecoder('utf-8');
+	var buffer = '';
+	req.on('data', function(data) {
+		buffer += decoder.write(data);
+	});
+	req.on('end', function() { 
+		buffer += decoder.end();
+
+		// send the response 
+		res.end('Hello World\n');
+
+		// log the requested path
+		//console.log('Request recieved on path: ' + trimmedPath + ' with method: ' + method + ' and with these query string parameters ', queryStringObject);
+		console.log('Request recieved with this payload: ', buffer);
 		
+	});
 
 });
 
